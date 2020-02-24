24ea7680541ed9a2c5772950816d9a8e9f67bdfc
victoria <bgreen.victoria@gmail.com>
2020-02-23 20:51:26 -0500

    Created subtask store and controllers

---
+++ DAL/ISubtaskStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskAway.DAL.Model;
+
+namespace TaskAway.DAL
+{
+    public interface ISubtaskStore
+    { 
+        void AddSubtask(SubtasksDALModel model);
+        void DeleteSubtask(int id);
+        SubtasksDALModel EditSubtask(SubtasksDALModel model);
+        IEnumerable<SubtasksDALModel> SelectAllSubtasks();
+    }
+}
---
+++ DAL/SubtaskStore.cs
@@ -0,0 +1,31 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskAway.DAL.Model;
+
+namespace TaskAway.DAL
+{
+    public class SubtaskStore : ISubtaskStore
+    {
+        public void AddSubtask(SubtasksDALModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteSubtask(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SubtasksDALModel EditSubtask(SubtasksDALModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SubtasksDALModel> SelectAllSubtasks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
